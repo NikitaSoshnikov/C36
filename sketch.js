445cc54d8aa08a64523bb62e32cbbfac43e5a192
NikitaSoshnikov <72586065+NikitaSoshnikov@users.noreply.github.com>
2021-01-21 18:56:20 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var database, form, player, game, playerCount;
+var gameState = 0 
+
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+  game = new Game()
+    game.getState()
+  game.start()
+  
+
+  }
+
+function draw(){
+  
+  
+}
+
